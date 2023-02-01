0a676415261e6a0657df6f30e810e506d37c3846
grynka <ihor.kozhemyakin@gmail.com>
2023-02-02 00:37:46 +0200

    upd

--- tsCourse/app.js
+++ tsCourse/app.js
@@ -1,9 +1,18 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+"use strict";
+const page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: '2021-01-01',
+        updateAt: '2021-05-01',
+    }
+};
+const page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+};
+//# sourceMappingURL=app.js.map
--- tsCourse/app.ts
+++ tsCourse/app.ts
@@ -1,11 +1,28 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-function add(num1: number, num2: number) {
-  return num1 + num2;
+type Page = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: 'open' | 'close',
+  details?: {
+    createAt: string,
+    updateAt: string,
+  }
 }
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+const page1: Page = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  }
+}
+
+const page2: Page = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
