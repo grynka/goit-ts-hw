97969d95b35b7968529dc86cd969cf0f94b1c82e
grynka <ihor.kozhemyakin@gmail.com>
2023-02-02 18:29:09 +0200

    Update app.ts

--- tsCourse/app.ts
+++ tsCourse/app.ts
@@ -1,28 +1,5 @@
-type Page = {
-  title: string,
-  likes: number,
-  accounts: string[],
-  status: 'open' | 'close',
-  details?: {
-    createAt: string,
-    updateAt: string,
-  }
-}
+class House {
+  door: open | close;
+  key: {} = {}
 
-const page1: Page = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: '2021-01-01',
-    updateAt: '2021-05-01',
-  }
-}
-
-const page2: Page = {
-  title: 'Python or Js',
-  likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
 }
